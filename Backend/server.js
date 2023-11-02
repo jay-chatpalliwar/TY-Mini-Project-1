30f130793df64622b510e8ac8b303dcd2a55aa0a
Bhupendra Jambhale <110332293+bhupendraj9@users.noreply.github.com>
2023-11-02 21:49:58 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const cors = require('cors');
-
-const app = express();
-app.use(express.json());
+const cloudConnect = require('./config/cloudConnect');
+const cookieParser = require('cookie-parser');
+const routes = require('./routes/routes');
+const fileUpload = require('express-fileupload');
 require('dotenv').config();
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser());
+app.use(fileUpload({
+    useTempFiles : true,
+    tempFileDir : '/tmp/'
+}));
+app.use(routes);
 
 // console.log(process.env.PORT);
 const port = process.env.PORT || 4000;
 app.listen(port ,() =>{
     console.log("App listening on port : " + port);
 });
-app.use(cors());
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
+//using express json so commented
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.json());
 
 const {addMessage} = require('./controllers/addMessage');
 app.post('/addMessage',addMessage);
