332db50e64daeb32e8f9c4d7596e5024fa4c6046
Bhupendra Jambhale <110332293+bhupendraj9@users.noreply.github.com>
2023-11-03 22:38:34 +0530

    Update UserDetails.js

--- Backend/controllers/common/UserDetails.js
+++ Backend/controllers/common/UserDetails.js
@@ -1,1 +1,32 @@
 
+const { useState } = require("react");
+const User = require("../../models/User");
+
+exports.UserDetails = async(req,res)=>{
+   try{
+ 
+     const email = req.body.email;
+      const token = req.body.token;
+      if(!email) return res.status(400).json({
+      success:false,
+      message:'Try to login again'
+      })
+      
+      const user = await User.findOne({email:email})
+      if(!user) return res.status(400).json({success:false, message:' No user found'});
+      
+      return res.status(200).json(
+      {
+       success:true,
+       user:user
+      })
+      
+   } 
+   catch(e)
+   {
+    return res.status(500).json({
+    success:false,
+    message:'Error in getting user info'
+    })
+   }
+}
